ad105ab5dbe1961042c79a1ccb165eafe5896438
Dmitry Melikhov <dima.mel8@gmail.com>
2021-03-16 22:34:10 +0200

    Hmm

--- domain/src/main/java/dimas/jpa/sandbox/domain/user/UserService.java
+++ domain/src/main/java/dimas/jpa/sandbox/domain/user/UserService.java
@@ -1,25 +1,12 @@
 package dimas.jpa.sandbox.domain.user;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.lang.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-
 @Service
-@Slf4j
-public class UserService implements ApplicationRunner {
+@RequiredArgsConstructor
+public class UserService {
 
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        hello("aa");
-        hello(null);
-        hello(Map.of("foo", "bar").get("baz"));
-    }
-
-    private void hello(@NonNull Object world) {
-        log.info("world: {}", world.toString());
+    void hello() {
     }
 }
